a65b54803ab61b8ba1253f18f66c2fbde7df809f
forest <skt.P015062@partner.sk.com>
2021-06-16 13:48:34 +0900

    문구 수정 테스트

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text>expo 짱짱맨</Text>
       <StatusBar style="auto" />
     </View>
   );
