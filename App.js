963bbccbbe0a9fdb10af579262ef9fdd5762fe73
forest <skt.P015062@partner.sk.com>
2021-06-16 14:20:04 +0900

    first commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>expo 짱짱맨</Text>
+      <Text>expo 짱짱맨222</Text>
       <StatusBar style="auto" />
     </View>
   );
